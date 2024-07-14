2cbe830176f352a5192588af9b8b05d8ca3075da
inmare <yatsumehole@naver.com>
2024-07-14 19:13:08 +0900

    잘못된 변수 및 함수 이름 수정

--- playlist_generator/playlist.py
+++ playlist_generator/playlist.py
@@ -9,12 +9,14 @@ import zipfile
 def zip_files(info_list, data_type, zipf):
     if data_type == DlDataType.MUSIC:
         folder_name = config.FOLDER_NAME["music"]
+        ext = "mp3"
     elif data_type == DlDataType.THUMBNAIL:
         folder_name = config.FOLDER_NAME["thumbnail"]
+        ext = "png"
 
     for info in info_list:
         file_name = info[config.ADDITIONAL_KEY["파일 이름"]]
-        zipf.write(f"{folder_name}/{file_name}")
+        zipf.write(f"{folder_name}/{file_name}.{ext}")
 
 
 def add_dl_status(info, data_type, dl_status):
@@ -33,10 +35,12 @@ def get_dl_status(info, data_type, is_json_exist):
         is_dl_success = json_data.check_dl_status(info, data_type)
         if not is_dl_success:
             dl_status = download_data(info, data_type, retry_dl=True)
+            return dl_status
+        else:
+            return [True] * len(info)
     else:
         dl_status = download_data(info, data_type)
-
-    return dl_status
+        return dl_status
 
 
 def generate(excel_file_name: str):
@@ -45,20 +49,20 @@ def generate(excel_file_name: str):
     # 날짜가 포함된 json 파일이 있는지 체크하기
     is_json_exist = json_data.check_json_file_exist(date)
     if is_json_exist:
-        info = json_data.read_json_data(date, is_json_exist)
+        info = json_data.read_json_data(date)
     else:
         info = excel_data.get_info(recommend_sheet)
     music_dl_status = get_dl_status(info, DlDataType.MUSIC, is_json_exist)
     thumbnail_dl_status = get_dl_status(info, DlDataType.THUMBNAIL, is_json_exist)
 
-    dl_success = not False in [music_dl_status, thumbnail_dl_status]
+    dl_success = not False in music_dl_status and not False in thumbnail_dl_status
 
     if dl_success:
         # json 파일에 데이터 쓰고 압축하기
         json_data.write_json_data(date, info)
         log_print(LogType.SUCCESS, "모든 데이터를 다운로드 받았습니다.")
-        zipf = zipfile.Zipfile(
-            f"{date.year}-{date.month}-{config.ZIP_FILE_SUFFIX}.zip",
+        zipf = zipfile.ZipFile(
+            f"{date.year}-{str(date.month).zfill(2)}-{config.ZIP_FILE_SUFFIX}.zip",
             "w",
             zipfile.ZIP_DEFLATED,
         )
@@ -66,6 +70,14 @@ def generate(excel_file_name: str):
         zip_files(info, DlDataType.THUMBNAIL, zipf)
         zipf.write(json_data.get_json_file_name(date))
         zipf.close()
+        log_print(
+            LogType.SUCCESS,
+            f"{date.year}년 {date.month}월 플레이리스트 압축파일을 생성했습니다.",
+        )
+        log_print(
+            LogType.SUCCESS,
+            f"생성된 압축파일 이름은 {date.year}-{str(date.month).zfill(2)}-{config.ZIP_FILE_SUFFIX}.zip 입니다.",
+        )
     else:
         # json 파일에 데이터 쓰기만 하기
         if False in music_dl_status:
@@ -76,6 +88,10 @@ def generate(excel_file_name: str):
         log_print(
             LogType.WARNING,
             "모든 데이터를 다운로드 받지 못했습니다. 다시 프로그램을 시도해서 누락된 파일을 다운로드 하세요.",
+        )
+        log_print(
+            LogType.WARNING,
+            "혹은 생성된 모든 폴더와 파일을 삭제하고 엑셀 파일의 링크 정보를 수정한 후 다시 시도하세요.",
         )
 
 
