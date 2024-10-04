2964f0cd12c022422f5dfb92a1dadf6a086b64a9
inmare <yatsumehole@naver.com>
2024-10-04 14:09:38 +0900

    CustomException 클래스 추가

--- playlist_generator/error.py
+++ playlist_generator/error.py
@@ -1,16 +1,35 @@
-class CellValueError(Exception):
-    def __init__(self, person, prop):
-        self.msg = f"{person}님의 {prop}에 해당하는 정보가 없는 것 같습니다. 엑셀 파일을 확인해주세요."
+# class CellValueError(Exception):
+#     def __init__(self, person, prop):
+#         self.msg = f"{person}님의 {prop}에 해당하는 정보가 없는 것 같습니다. 엑셀 파일을 확인해주세요."
 
-    def __str__(self):
-        return self.msg
+#     def __str__(self):
+#         return self.msg
 
 
-class UserInvokedError(Exception):
-    def __init__(self, msg):
+# class UserInvokedError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+
+#     def __str__(self):
+#         return self.msg
+
+
+class CustomException(Exception):
+    """
+    사용자가 정의한 에러클래스
+    해당 에러가 발생하면 사용자에게 메시지를 출력하고 프로그램을 종료한다.
+    만약 예상하지 못한 에러가 발생한다면 그 에러의 메세지를 그대로 출력한다.
+    """
+
+    def __init__(self, msg: str) -> None:
+        """
+        에러클래스 생성자
+
+        :param msg: 에러 메시지
+        """
         self.msg = msg
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.msg
 
 
