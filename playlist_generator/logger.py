2610cbb0e070981c22a68c3bd8216eccdc2b3251
inmare <yatsumehole@naver.com>
2024-10-05 23:31:37 +0900

    불필요한 주석 제거 및 콘솔 텍스트 설정 추가

--- playlist_generator/logger.py
+++ playlist_generator/logger.py
@@ -61,26 +61,5 @@ class Logger:
         cls.logger.error(msg)
 
 
-# from enum import Enum, auto
-#
-#
-# class LogType(Enum):
-#     ERROR = auto()
-#     WARNING = auto()
-#     SUCCESS = auto()
-#     PROGRESS = auto()
-#
-#
-# def log_print(log_type, msg):
-#     if log_type == LogType.SUCCESS:
-#         print(f"{Fore.GREEN}성공:{Style.RESET_ALL}{msg}")
-#     elif log_type == LogType.PROGRESS:
-#         print(f"{Fore.BLUE}작업:{Style.RESET_ALL}{msg}")
-#     elif log_type == LogType.ERROR:
-#         print(f"{Fore.RED}오류:{Style.RESET_ALL}{msg}")
-#     elif log_type == LogType.WARNING:
-#         print(f"{Fore.YELLOW}경고:{Style.RESET_ALL}{msg}")
-
-
 if __name__ == "__main__":
     pass
